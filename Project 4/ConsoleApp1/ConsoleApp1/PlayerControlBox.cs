c8cedcd1887b16bd479f8556e158f5273c16a42f
HusackJo <husackjo@gmail.com>
2024-03-30 23:58:46 -0400

    Update PlayerControlBox.cs

--- Project 4/ConsoleApp1/ConsoleApp1/PlayerControlBox.cs
+++ Project 4/ConsoleApp1/ConsoleApp1/PlayerControlBox.cs
@@ -3,6 +3,10 @@ using System.Numerics;
 
 namespace ConsoleApp1
 {
+    /* Class for the "hitboxes" or what players are gonna use to tap the hit tiles. the arrow shaped boxes that show up in DDR.
+     * Update() is gonna play every frame. Draws the boxes and does a HitCheck[er]
+     * 
+     */
     internal class PlayerControlBox
     {
         //Variables Baby!! 
@@ -12,7 +16,7 @@ namespace ConsoleApp1
         {
             //give those variables some values!
         }
-        public void HitChecker(HitTile hitTile)
+        public void HitCheck(HitTile hitTile)
         {
             if (/*they line up*/true)
             {
@@ -20,8 +24,17 @@ namespace ConsoleApp1
             }
             else
             {
-                //they miss! point tracker runs a function here?
+                //they miss! run a public function in the point tracker here?
             }
         }
+        public void Update()
+        {
+            //
+            // if (keydown)
+            //HitChecker(corresponding hittile);
+            //draw
+            Raylib.DrawRectangleLines(int.Parse(position.X.ToString()), int.Parse(position.Y.ToString()), int.Parse(size.X.ToString()), int.Parse(size.Y.ToString()), Color.SkyBlue);
+        }
+
     }
 }
