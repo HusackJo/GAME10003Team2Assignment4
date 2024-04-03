4c17de960786b8dc18054a1cba63e1355a923875
mbarrameda <161363276+mbarrameda@users.noreply.github.com>
2024-04-02 21:45:24 -0400

    hi! tried to add timer and background music but it doesn't work? :( - matt

---
+++ Project 4/ConsoleApp1/ConsoleApp1/Timer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Xml;
+using Raylib_cs;
+
+class Program
+{
+    static void Timer(string[] args)
+    {
+        const int screenWidth = 800;
+        const int screenHeight = 450;
+
+        Raylib.InitWindow(screenWidth, screenHeight, "Automatic Timer");
+
+        int seconds = 0;
+        DateTime startT = DateTime.Now;
+
+        Raylib.SetTargetFPS(60);
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+
+            TimeSpan elapsedTime = DateTime.Now - startT;
+            seconds = (int)elapsedTime.TotalSeconds;
+
+            Raylib.DrawText($"Time elapsed: {seconds} seconds", 10, 10, 20, Color.Black);
+
+            Raylib.EndDrawing();
+
+        }
+
+        Raylib.CloseWindow();
+    }
+}
